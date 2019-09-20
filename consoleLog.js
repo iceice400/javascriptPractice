5db15771f1db105b2e072f50b8f55a8a1f6f3f26
iceice400 <AdamAndrew2468@gmail.com>
2019-09-20 19:45:01 -0400

    Added descriptions for fizzbuzz and consolelop

---
+++ consoleLog.js
@@ -0,0 +1,4 @@
+/**
+ * consoleLog
+ * Write a function that using console.log to log every number from 1-100
+ */
---
+++ fizzBuzz.js
@@ -0,0 +1,7 @@
+/**
+ * FizzBuzz
+ * write a function that when given an integer returns
+ * "fizz" if the number is divisible by 3
+ * "buzz" if the number is divisible by 5
+ * and "fizzbuzz" if the number is divisible by both 3 and 5
+ */
