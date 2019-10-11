c08f8cb7e3b7fac33ec3556a56188267a6533e99
iceice400 <AdamAndrew2468@gmail.com>
2019-10-11 16:19:25 -0400

    javaScript login

---
+++ login.js
@@ -0,0 +1,19 @@
+let attempt = 4;
+function validate() {
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  if (username == "Admin" && password == "DefaultPassword") {
+    alert("Login Successfully");
+    window.location = "success.html";
+    return false;
+  } else {
+    attempt--;
+    alert("You have left " + attempt + " attempt;");
+    if (attempt == 0) {
+      document.getElementById("username").disabled = true;
+      document.getElementById("password").disabled = true;
+      document.getElementById("submit").disabled = true;
+      return false;
+    }
+  }
+}
